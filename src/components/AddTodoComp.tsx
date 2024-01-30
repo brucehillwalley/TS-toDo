aa88242b7dbb87927928ba0d855f80c255d79f2b
BruceHill <brucehillwalley@gmail.com>
2024-01-30 13:16:12 +0300

    fix:warning

--- src/components/AddTodoComp.tsx
+++ src/components/AddTodoComp.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, TextField } from '@mui/material'
-import React, { useState } from 'react'
-import SaveIcon from '@mui/icons-material/Save';
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import SaveIcon from "@mui/icons-material/Save";
 
 // const AddTodoComp = ({addTodo}:{addTodo:(task:string)=>Promise<void>}) => {
 //     // const [task,setTask] = useState<string>('')
@@ -46,53 +46,54 @@ import SaveIcon from '@mui/icons-material/Save';
 
 //? 2.kullanım
 
-interface IAddTodoComp{
-// addTodo:(task:string)=>Promise<void>
-addTodo:AddFn
-//*başka proplar eklenmek isterse buraya eklenebilir.
+interface IAddTodoComp {
+  // addTodo:(task:string)=>Promise<void>
+  addTodo: AddFn;
+  //*başka proplar eklenmek isterse buraya eklenebilir.
 }
 
-
-const AddTodoComp = ({addTodo}:IAddTodoComp) => {
+const AddTodoComp = ({ addTodo }: IAddTodoComp) => {
   // const [task,setTask] = useState<string>('')
-  const [task,setTask] = useState('') //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
+  const [task, setTask] = useState(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
 
   const handleClick = () => {
-      console.log(task)
-      addTodo(task)
-      setTask("")
-  }
-return (
-  <div><Box
-  sx={{
-    display: { xs: "block", sm: "flex" },
-    justifyContent: { xs: "flex-start", sm: "center" },
-    m: { xs: 1, sm: "auto" },
-    height: { xs: "120px", sm: "80px" },
-  }}
->
-  <TextField
-    id="outlined-basic"
-    label="New Todo"
-    color="success"
-    sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
-    variant="outlined"
-    value={task}
-    onChange={(e) => setTask(e.target.value)}
-    inputProps={{maxLength:40}}
-  />
-  <Button
-    variant="contained"
-    color="success"
-    sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
-    endIcon={<SaveIcon />}
-    disabled={!task.trim()}
-    onClick={handleClick}
-  >
-    Save Todo
-  </Button>
-</Box></div>
-)
-}
+    console.log(task);
+    addTodo(task);
+    setTask("");
+  };
+  return (
+    <div>
+      <Box
+        sx={{
+          display: { xs: "block", sm: "flex" },
+          justifyContent: { xs: "flex-start", sm: "center" },
+          m: { xs: 1, sm: "auto" },
+          height: { xs: "120px", sm: "80px" },
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="New Todo"
+          color="success"
+          sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+          variant="outlined"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          inputProps={{ maxLength: 40 }}
+        />
+        <Button
+          variant="contained"
+          color="success"
+          sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+          endIcon={<SaveIcon />}
+          disabled={!task.trim()}
+          onClick={handleClick}
+        >
+          Save Todo
+        </Button>
+      </Box>
+    </div>
+  );
+};
 
-export default AddTodoComp
+export default AddTodoComp;
